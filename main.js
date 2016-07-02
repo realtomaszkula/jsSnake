34f4f1aa0ff5902f888a8ff6b5e55292e188db20
Tomasz Kula <tomaszkula07@gmail.com>
2016-07-02 15:16:47 +0200

    Snake can move!

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-function changeDirection(){
+  var snake = {
+    direction : 'r',
+    body : [[20,20],[20,19]],
+  };
 
+
+function changeDirection(){
 }
 
-function drawSnake(snakeBody){
+function getInput(){
+  var key = '';
+  $('body').keydown(function(event) {
+    switch(event.which) {
+        case 38 : key = 'u'; break;
+        case 39 : key = 'r'; break;
+        case 40 : key = 'd'; break;
+        case 41 : key = 'l'; break;
+    }
+  });
+  return key;
+}
+
+function renderBoard() {
   var $container = $('#container');
 
-  snakeBody.forEach(function(entry){
+  //drawing board
+  for(var x = 1; x < 41; x++) {
+    for(var y = 1; y < 41; y++) {
+      $container.append('<div class="grid x" id="' +
+        x + '_' + y + '"></div>');
+    }
+  }
+}
+
+function renderSnake() {
+  // remove old snake
+  $('.snake').removeClass('snake');
+
+  // draw new snake
+  snake.body.forEach(function(entry){
     var x = entry[0];
     var y = entry[1];
 
@@ -15,24 +47,48 @@ function drawSnake(snakeBody){
   });
 }
 
-function render() {
-  var $container = $('#container');
-  for(var x = 1; x < 41; x++) {
-    for(var y = 1; y < 41; y++) {
-      $container.append('<div class="grid x" id="' + x + '_' + y + '"></div>');
-    }
+function move(){
+  var body = snake.body;
+  var newPosition;
+
+  switch (snake.direction) {
+    case 'r' :
+      newPosition = [body[0][0] + 1, body[0][1]];
+      break;
+    case 'l' :
+      newPosition = [body[0][0] - 1, body[0][1]];
+      break;
+    case 'u' :
+      newPosition = [body[0][0], body[0][1] + 1];
+      break;
+    case 'd' :
+      newPosition = [body[0][0], body[0][1] - 1];
+      break;
   }
+
+  snake.body = [newPosition, ...body];
+  snake.body.pop();
+}
+
+function gameOver(){
+  var body = snake.body[0];
+  return body[0] > 40  ||  body[0] < 0 ||
+            body[1] > 40  ||  body[1] < 0;
 }
 
 $(document).ready(function(){
-  var snake = {
-    direction : 'r',
-    body : [[20,20]],
-  };
+    renderBoard();
+    setInterval(function() {
+        renderSnake();
+        move();
+        if (gameOver()) return;
+      },
+        250
+    );
 
-  render();
-  drawSnake(snake.body);
 });
+
+
 
 
 
